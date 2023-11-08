3e11324483e51fe559fd1b701218d9b06c6f91d9
ugatai <ugawa.service@gmail.com>
2023-11-08 22:36:59 +0900

    :cop: :clean: 開発ブランチ rubocopフォーマット実行

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -15,13 +15,13 @@ require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
   context '必須パラメータが入力されている場合' do
-    it "保存処理ができる" do
+    it '保存処理ができる' do
       admin = create(:valid_admin)
       expect(admin).to be_valid
     end
   end
   context '必須パラメータにnilがある場合' do
-    it "保存処理ができない" do
+    it '保存処理ができない' do
       admin = build(:invalid_admin)
       expect(admin).to_not be_valid
     end
--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -16,14 +16,14 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  context "必須パラメータが入力されている場合" do
-    it "保存処理ができる" do
+  context '必須パラメータが入力されている場合' do
+    it '保存処理ができる' do
       customer = create(:valid_customer)
       expect(customer).to be_valid
     end
   end
-  context "必須パラメータにnilがある場合" do
-    it "保存処理ができない" do
+  context '必須パラメータにnilがある場合' do
+    it '保存処理ができない' do
       customer = build(:invalid_customer)
       expect(customer).to_not be_valid
     end
