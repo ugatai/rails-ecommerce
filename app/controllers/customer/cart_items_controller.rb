59074410cfa13db15cab9a2f3f32070eb040e1fe
ugatai <ugawa.service@gmail.com>
2023-11-10 15:37:38 +0900

    :sparkles: :feat: カート機能 合計金額ロジックの追加 #7

--- app/controllers/customer/cart_items_controller.rb
+++ app/controllers/customer/cart_items_controller.rb
@@ -4,6 +4,7 @@ class Customer::CartItemsController < ApplicationController
 
   def index
     @cart_items = current_customer.cart_items
+    @total = @cart_items.inject(0) { |sum, cart_item| sum + cart_item.line_total }
   end
 
   def create
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -12,4 +12,8 @@
 class CartItem < ApplicationRecord
   belongs_to :customer
   belongs_to :product
+
+  def line_total
+    product.price * quantity
+  end
 end
