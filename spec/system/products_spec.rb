fa9a0708184dcc02bf1a65d50d2a115e9880de98
ugatai <ugawa.service@gmail.com>
2023-11-10 10:16:42 +0900

    :bug: :fix: 開発ブランチ 既存のシステムテストバグに対応

--- spec/system/products_spec.rb
+++ spec/system/products_spec.rb
@@ -1,9 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe 'Products', type: :system do
-  before do
-    driven_by(:rack_test)
-  end
+  describe '管理者ログイン' do
 
-  pending "add some scenarios (or delete) #{__FILE__}"
+    let(:admin) { create(:valid_admin) }
+
+    before do
+      allow(self).to receive(:visit)
+      allow(self).to receive(:fill_in)
+      allow(self).to receive(:click_button)
+      login_as(admin)
+    end
+
+    it '管理画面側で商品の追加ができる' do
+      visit new_admin_product_path
+
+      fill_in 'Name', with: '追加商品'
+      fill_in 'Description', with: '商品説明がここに入ります。商品説明がここに入ります。商品説明がここに入ります。'
+      fill_in 'Price', with: 1000
+      fill_in 'Stock', with: 5
+      click_button 'Create Product'
+
+      expect(page).to have_content('追加商品')
+      expect(page).to have_content('商品説明がここに入ります。商品説明がここに入ります。商品説明がここに入ります。')
+      expect(page).to have_content('¥1,000')
+      expect(page).to have_content('In stock (5)')
+    end
+
+    it '管理画面側で商品の編集ができる' do
+      visit edit_admin_product_path(Product.last)
+
+      fill_in 'Name', with: '更新追加商品'
+      fill_in 'Description', with: '商品説明がここに入ります。商品説明がここに入ります。商品説明がここに入ります。'
+      fill_in 'Price', with: 1000
+      fill_in 'Stock', with: 12
+      click_button 'Edit Product'
+
+      expect(page).to have_content('更新追加商品')
+      expect(page).to have_content('商品説明がここに入ります。商品説明がここに入ります。商品説明がここに入ります。')
+      expect(page).to have_content('¥1,000')
+      expect(page).to have_content('In stock (12)')
+    end
+  end
 end
